6d3aa6740a97405c06ba1137bf91fb9b7f800b44
Yandra Muslim <yandrayt1@gmail.com>
2023-06-04 22:01:23 +0700

    updating user

--- src/components/atoms/Search.jsx
+++ src/components/atoms/Search.jsx
@@ -3,8 +3,12 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const Search = () => {
   return (
-    <div className="flex items-center px-6 py-2 text-black bg-white rounded-full">
-      <input type="text" placeholder="Cari..." className="outline-none" />
+    <div className="flex items-center px-6 py-2 bg-white rounded-full dark:text-black">
+      <input
+        type="text"
+        placeholder="Cari..."
+        className="outline-none dark:text-black"
+      />
       <SearchIcon style={{ color: "black" }} className="cursor-pointer" />
     </div>
   );
--- src/pages/user/UserHome.jsx
+++ src/pages/user/UserHome.jsx
@@ -11,37 +11,41 @@ const UserHome = () => {
     });
   }, []);
   return (
-    <div className="relative flex flex-col justify-center min-h-screen bg-cover items-centerter bg-primary dark:bg-slate-900">
-      <div className="absolute justify-between hidden w-full h-full overflow-x-hidden md:flex ">
-        <img src="./doctor.png " alt="doctor" />
-        <img src="./woman.png" alt="doctor" />
-      </div>
-      <div className="relative flex flex-col items-center justify-center w-full h-full ">
+    <>
+      <div className="relative flex flex-col items-center justify-center min-h-screen bg-primary dark:bg-slate-900">
         <img
-          className="md:w-[600px] w-[500px] px-3 md:px-0"
-          src="./heroAsset.png"
-          alt="hero"
+          src="./hero.png"
+          alt="doctor"
+          className="absolute md:bottom-0 w-[1050px] 2xl:w-[1400px]"
         />
-        <h1 className="absolute text-2xl font-extrabold text-center dark:text-white md:text-5xl top-10 md:top-14 ">
-          Meddycine <br />
-          Teddy
-        </h1>
-        <p className="absolute max-w-xs text-sm text-center dark:text-white md:max-w-lg md:text-lg">
-          Meddy membantu pengguna untuk mencari informasi dengan lebih mudah dan
-          efisien serta memantau dan mengelola konsumsi obat dengan baik.
-        </p>
-        <div className="absolute flex gap-10 bottom-16">
-          <Button
-            content={"Tentang"}
-            style="bg-white px-5 md:px-12 shadow-2xl hover:bg-primary hover:text-white"
+        <div className="relative flex flex-col items-center justify-center w-full h-full ">
+          <img
+            className="md:w-[600px] w-[500px] px-3 md:px-0"
+            src="./heroAsset.png"
+            alt="hero"
           />
-          <Button
-            content={"Unduh"}
-            style="bg-white px-5 md:px-12 shadow-2xl hover:bg-primary hover:text-white"
-          />
+          <h1 className="absolute text-2xl font-extrabold text-center dark:text-white md:text-5xl top-10 md:top-14 ">
+            Meddycine <br />
+            Teddy
+          </h1>
+          <p className="absolute max-w-xs text-sm text-center dark:text-white md:max-w-lg md:text-lg">
+            Meddy membantu pengguna untuk mencari informasi dengan lebih mudah
+            dan efisien serta memantau dan mengelola konsumsi obat dengan baik.
+          </p>
+          <div className="absolute flex gap-10 bottom-16">
+            <Button
+              content={"Tentang"}
+              style="bg-white px-5 dark:text-black md:px-12 shadow-2xl hover:bg-primary hover:text-white"
+            />
+            <Button
+              content={"Unduh"}
+              style="bg-white px-5 dark:text-black md:px-12 shadow-2xl hover:bg-primary hover:text-white"
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="min-h-screen bg-secondary">test</div>
+    </>
   );
 };
 
