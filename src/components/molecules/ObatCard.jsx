a62d9776eef6fa29b88e8ff47dd7b8ec050b8795
Ravenaaul23 <ravenaauliya@gmail.com>
2023-06-15 00:42:34 +0700

    update

--- src/components/molecules/ObatCard.jsx
+++ src/components/molecules/ObatCard.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const BeritaCard = ({ url, alt, date, title }) => {
   return (
-    <div className="flex flex-col gap-12 p-4 shadow-lg w-72 h-[350px] dark:bg-slate-900 rounded-xl border-primary border">
+    <div className="flex flex-col gap-5 p-4 shadow-lg w-[270px] h-[350px] dark:bg-slate-900 rounded-xl border-primary border">
       <img src={url} alt={alt} />
       <Link
         to={`/berita/1`}
-        className="flex flex-col gap-3 text-xl font-bold text-center transition-all duration-200 cursor-pointer hover:text-primary"
+        className="flex flex-col gap-2 text-xl font-bold text-center transition-all duration-200 cursor-pointer hover:text-primary"
       >
         {title}
         <span className="text-primary">Sirup</span>
